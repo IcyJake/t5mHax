d567e5025298d1b9dda2e76eba24dec4315e5abf
IcyJake <icyyjakee@gmail.com>
2021-03-18 23:14:02 -0400

    Add all my files

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Figgle;
+
+namespace t5mHax
+{
+    class Program
+    {
+        static void ascii(string text)
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render(text));
+        }
+        static void Main(string[] args)
+        {
+            Memory.Mem m = new Memory.Mem();
+            Console.Title = "t5mHax";
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            ascii("t5mHax");
+            Console.WriteLine("\n\nMessage IcyJake#1200 on Discord for help/questions.");
+            Console.Write("\nPlease enter the amount of money you would like: ");
+
+            int amountOfMoney = int.Parse(Console.ReadLine());
+
+            m.OpenProcess(Process.GetProcessesByName("t5m").FirstOrDefault().Id);
+
+            if (amountOfMoney > 2147483647)
+            {
+                Console.Write("Please enter a little bit of a smaller number: ");
+
+                int amountOfMoney2 = int.Parse(Console.ReadLine());
+
+                if (amountOfMoney2 > 2147483647)
+                {
+                    Console.WriteLine("\nAn error has occurred! Please restart t5mHax.");
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    m.WriteMemory("t5m.exe+024C4FE0,A0", "int", Convert.ToString(amountOfMoney2));
+                    Console.WriteLine("\nSuccessfully transfered: $" + amountOfMoney2);
+                    Console.ReadKey();
+                }
+            }
+
+            else
+            {
+                m.WriteMemory("t5m.exe+024C4FE0,A0", "int", Convert.ToString(amountOfMoney));
+                Console.WriteLine("\nSuccessfully transfered: $" + amountOfMoney);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("t5mHax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("t5mHax")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1293c79e-fce1-4bab-a6bc-1d28aef9f1af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
